dd393c54cf6b3ea5d659d3133b66383f9f6de1a3
iwan280481 <159804304+iwan280481@users.noreply.github.com>
2024-02-12 23:17:42 +0700

    Add files via upload

---
+++ dbconfig.php
@@ -0,0 +1,12 @@
+<?php
+
+try {
+    $con = new PDO('mysql:host=localhost;dbname=asetnegla62', 'root', '', array(PDO::ATTR_PERSISTENT => true));
+} catch (PDOException $e) {
+    echo $e->getMessage();
+}
+
+include_once 'Auth.php';
+
+$user = new Auth($con);
+?>
